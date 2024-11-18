99aeef4cf40d5ca7c58a25e8a502a7590a00e29a
Syed Naeem Shah <naeemultimate@gmail.com>
2024-11-18 10:45:44 +0000

    How many vowels exercise

--- How-Many-Vowels.js
+++ How-Many-Vowels.js
@@ -1,3 +1,8 @@
 function countVowels(word){
-    
+    const regex = /[a,e,i,o,u]/g;
+    const found = word.match(regex)
+    return found.length;
 }
+
+console.log(countVowels('Celebration')); // Should return 5
+console.log(countVowels('Hello')); // Should return 2
