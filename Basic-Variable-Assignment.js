2d119839a2c70149c24aac75f140e866393b69b5
Syed Naeem Shah <naeemultimate@gmail.com>
2024-12-09 01:00:41 +0000

    Minor adjustment on challenge

--- Basic-Variable-Assignment.js
+++ Basic-Variable-Assignment.js
@@ -1,6 +1,7 @@
 function nameString(name){
-    let result = name + 'Edabit';
+    let result = name + ' ' + 'Edabit'; //String concatenation
     return result;
 }
 
-console.log(nameString("Matt")); // Should return result MattEdabit
+console.log(nameString("Matt")); // Should return result Matt Edabit
+console.log(nameString("Naeem")); // Should return result Naeem Edabit
